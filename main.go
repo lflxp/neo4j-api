689d977931efbef97214afe6223fb50ce5ca9be5
lixueping <lixueping@cloudwalk.cn>
2019-01-25 10:42:05 +0800

    init

---
+++ main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	_ "github.com/lflxp/neo4j-api/routers"
+
+	"github.com/astaxie/beego"
+)
+
+func main() {
+	if beego.BConfig.RunMode == "dev" {
+		beego.BConfig.WebConfig.DirectoryIndex = true
+		beego.BConfig.WebConfig.StaticDir["/swagger"] = "swagger"
+	}
+	beego.Run()
+}
